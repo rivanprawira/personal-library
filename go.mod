c13718b6984a0c8be4a579333caf817441f49c57
Muhammad Rivan Prawira Gustha <prawiraa.rivan@gmail.com>
2025-02-13 09:01:31 +0700

    Version 2

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module personal-library
 
 go 1.23.6
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,15 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"personal-library/routes"
+	"strings"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	routes.SetupRoutes()
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	fmt.Println("Server berjalan pada URL: http://localhost:9000")
-	log.Fatal(http.ListenAndServe(":9000", nil))
+	expectedUsername := os.Getenv("USERNAME_SERVER")
+	expectedPassword := os.Getenv("PASSWORD_SERVER")
+
+	fmt.Println("USERNAME:", expectedUsername)
+	fmt.Println("PASSWORD:", expectedPassword)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter username: ")
+	inputUsername, _ := reader.ReadString('\n')
+	inputUsername = strings.TrimSpace(inputUsername)
+	fmt.Print("Enter password: ")
+	inputPassword, _ := reader.ReadString('\n')
+	inputPassword = strings.TrimSpace(inputPassword)
+
+	if inputUsername != expectedUsername || inputPassword != expectedPassword {
+		log.Fatal("Invalid username or password")
+	}
+
+	routes.SetupRoutes()
+	port := os.Getenv("PORT")
+	log.Printf("Login successful. Starting server on URL: http://localhost:%s...", port)
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
