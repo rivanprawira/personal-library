2986f098d85a3045b870f7f01e8a21f654c60e27
Muhammad Rivan Prawira Gustha <prawiraa.rivan@gmail.com>
2025-02-17 15:12:37 +0700

    Update Goroutine

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ func main() {
 	server := &http.Server{Addr: ":" + port}
 
 	stopServer := make(chan struct{})
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
 	var wg sync.WaitGroup
